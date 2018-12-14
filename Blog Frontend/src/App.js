f90319301e502b49918cfb7aa7f325fa0c0fb80e
DESKTOP-9H4TH8F\tuomo <tuomo.eskola@vincit.fi>
2018-12-14 21:15:44 +0200

    Subtask 7.1 done

--- Blog Frontend/src/App.js
+++ Blog Frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Blog from './components/Blog'
 import BlogForm from './components/BlogForm'
 import blogService from './services/blogs'
+import userService from './services/users'
 import loginService from './services/login'
 import Notification from './components/Notification'
 import './App.css'
@@ -11,12 +12,14 @@ import {connect} from 'react-redux';
 import { 
     notify
  } from './reducers/blogReducer'
+import { BrowserRouter as Router, Route, Link, NavLink } from 'react-router-dom'
 
 class App extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
             blogs: [],
+            users: [],
             user: null,
             username: '',
             password: '',
@@ -32,9 +35,10 @@ class App extends React.Component {
             blogService.setToken(storedUser.token)
         }
         blogService.getAll().then(async blogs => {
-            await this.setState({ blogs })
-            console.log(this.state.blogs)
-            console.log(this.state.blogs[0].user.name)
+            this.setState({ blogs })
+        })
+        userService.getAll().then(async users => {
+            this.setState({ users })
         })
     }
 
@@ -114,39 +118,8 @@ class App extends React.Component {
         }
     }
 
-    render() {
-        if (this.state.user === null) {
-            return (
-                <div>
-                    <Notification.Notification message={this.props.notification} messageStyle="error-pop-up"/>
-                    <h2 className="loginTitle">Login to application</h2>
-                    <form className="loginForm">
-                        <div>
-                            username:
-                            <input
-                                type="text"
-                                name="username"
-                                value={this.state.username}
-                                onChange={this.handleLoginFieldChange}
-                                className="usernameField"
-                            />
-                        </div>
-                        <div>
-                            password:
-                            <input
-                                type="password"
-                                name="password"
-                                value={this.state.password}
-                                onChange={this.handleLoginFieldChange}
-                                className="passwordField"   
-                            />
-                        </div>
-                        <button onClick={this.login} className="loginButton">Login</button>
-                    </form>
-                </div>
-            )
-        }
-        return (
+    topBar = () => {
+        return(
             <div>
                 <h2>blogs</h2>
                 <p>
@@ -155,12 +128,95 @@ class App extends React.Component {
                 <Togglable.Togglable buttonLabel="create blog">
                     <BlogForm createBlog={this.createBlog}/>
                 </Togglable.Togglable>
+            </div>
+        )
+    }
+
+    loginPage = () => {
+        return (
+            <div>
+                <Notification.Notification message={this.props.notification} messageStyle="error-pop-up"/>
+                <h2 className="loginTitle">Login to application</h2>
+                <form className="loginForm">
+                    <div>
+                        username:
+                        <input
+                            type="text"
+                            name="username"
+                            value={this.state.username}
+                            onChange={this.handleLoginFieldChange}
+                            className="usernameField"
+                        />
+                    </div>
+                    <div>
+                        password:
+                        <input
+                            type="password"
+                            name="password"
+                            value={this.state.password}
+                            onChange={this.handleLoginFieldChange}
+                            className="passwordField"   
+                        />
+                    </div>
+                    <button onClick={this.login} className="loginButton">Login</button>
+                </form>
+            </div>
+        )
+    }
+
+    frontPage = () => {
+        if (this.state.user === null) {
+            return(<this.loginPage />)
+        }
+        return (
+            
+            <div>
+                <this.topBar />
                 {this.state.blogs.sort((a, b) => {return b.likes - a.likes}).map(blog =>
                     <Blog key={blog._id} id={blog._id} blog={blog} user={this.state.user} addLike={this.addLike} deleteBlog={this.deleteBlog}/>
                 )}
             </div>
         )
     }
+    
+    usersPage = () => {
+        if (this.state.user === null) {
+            return(<this.loginPage />)
+        }
+        return (
+            <div>
+                <this.topBar />
+                <h2>users</h2>
+                <table>
+                    <tr>
+                        <td></td>
+                        <td><h3>blogs added</h3></td>
+                    </tr>
+                    {this.state.users.map(user => 
+                        <tr> 
+                            <td>{user.name + ' '}</td>
+                            <td>{user.blogs.length}</td>
+                        </tr>
+                    )}
+                </table>
+            </div>
+        )
+    }
+
+    render() {
+        return(
+            <div className="container">
+            <Router>
+                <div>
+                    <Route exact path="/" render={() => <this.frontPage />} />
+                    <Route path="/users" render={() => <this.usersPage />} />
+                </div>
+            </Router>
+          </div>
+        )
+
+    }
+        
 }
 
 const mapStateToProps = (state) => {
